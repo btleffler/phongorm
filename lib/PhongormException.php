691c876a1d98720e9a11fa4cef4650d962015e39
btleffler <btleffler@gmail.com>
2014-06-29 22:26:45 -0400

    Add simple exception class

---
+++ lib/PhongormException.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * PhongormException
+ * =================
+ * Just an exception wrapper to make it
+ * easy to tell that Phongorm is the issue.
+ */
+
+namespace btleffler\Phongorm;
+
+class PhongormException extends \Exception {}
