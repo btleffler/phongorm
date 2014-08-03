65d5cef627802078ebd04c9dbac08020ef085e75
btleffler <btleffler@gmail.com>
2014-08-03 10:13:10 -0400

    Add function to get Collection class name from Document class

--- lib/Document.php
+++ lib/Document.php
@@ -20,4 +20,16 @@ class Document extends ArrayObject {
 		);
 	}
 
+	public static function collectionClassName () {
+		if (isset(static::$collectionClassName))
+			return static::$collectionClassName;
+
+		$possibleClassName = preg_replace("/Document$/i", '', get_called_class());
+
+		if (class_exists($possibleClassName))
+			return $possibleClassName;
+
+		return "Collection";
+	}
+
 }
