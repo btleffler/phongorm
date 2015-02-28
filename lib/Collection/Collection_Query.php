d34846d4fc7efd93312d8209e7aa4b9739d01714
btleffler <btleffler@gmail.com>
2015-02-28 10:00:27 -0500

    Stub out extra classes

---
+++ lib/Collection/Collection_Query.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Collection_Query class
+ * ==================================
+ * Setup a Phongorm Collection class
+ */
+
+namespace Phongorm\Collection;
+
+class Collection_Query {
+
+
+
+}
